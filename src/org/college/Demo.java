7ef529df91841dcd3b07e6a2c32080cacbb294d9
GladysGabriel <lgssujithra@gmail.com>
2023-07-25 13:07:23 -0700

    FirstCommit

---
+++ src/org/college/Demo.java
@@ -0,0 +1,15 @@
+package org.college;
+
+public class Demo {
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		Human obj=new Human();
+		obj.SetAge(30);
+		obj.setName("Reddy");
+//		obj.age=11;
+//		obj.name="Navin";
+		
+		System.out.println(obj.getName()+" : "+obj.getAge());
+	
+	}
+}
